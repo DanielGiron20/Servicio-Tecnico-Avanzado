021b7d871b61a7b0505d2317628b3c8c01dc50f9
EvelynSabillon <145521401+EvelynSabillon@users.noreply.github.com>
2024-07-28 21:06:21 -0600

    Form Compras

--- Proyecto DB2/Compras.Designer.cs
+++ Proyecto DB2/Compras.Designer.cs
@@ -34,18 +34,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(393, 297);
+            this.label1.Location = new System.Drawing.Point(262, 193);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.Size = new System.Drawing.Size(96, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Compras";
+            this.label1.Text = "FormularioCompras";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Compras
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(889, 653);
+            this.ClientSize = new System.Drawing.Size(593, 424);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Compras";
             this.Text = "Compras";
             this.ResumeLayout(false);
