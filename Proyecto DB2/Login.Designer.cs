c5f6e7c9939e8a576b51a0b38256da6eb0b77641
Kenneth <kennethmendez702@gmail.com>
2024-07-30 19:47:12 -0600

    Esta es una prueba - Kenneth

--- Proyecto DB2/Login.Designer.cs
+++ Proyecto DB2/Login.Designer.cs
@@ -45,9 +45,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Proyecto_DB2.Properties.Resources.Manutenção;
-            this.pictureBox1.Location = new System.Drawing.Point(102, 22);
+            this.pictureBox1.Location = new System.Drawing.Point(68, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(221, 217);
+            this.pictureBox1.Size = new System.Drawing.Size(147, 141);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -57,9 +58,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(24, 293);
+            this.label1.Location = new System.Drawing.Point(16, 190);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Servidor:";
             // 
@@ -68,9 +70,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(24, 342);
+            this.label2.Location = new System.Drawing.Point(16, 222);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 20);
+            this.label2.Size = new System.Drawing.Size(92, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Base de datos:";
             // 
@@ -79,9 +82,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(24, 393);
+            this.label3.Location = new System.Drawing.Point(16, 255);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Usuario:";
             // 
@@ -90,41 +94,46 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.Control;
-            this.label4.Location = new System.Drawing.Point(24, 443);
+            this.label4.Location = new System.Drawing.Point(16, 288);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 20);
+            this.label4.Size = new System.Drawing.Size(75, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "Contraseña:";
             // 
             // server
             // 
-            this.server.Location = new System.Drawing.Point(169, 284);
+            this.server.Location = new System.Drawing.Point(113, 185);
+            this.server.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.server.Name = "server";
-            this.server.Size = new System.Drawing.Size(213, 26);
+            this.server.Size = new System.Drawing.Size(143, 20);
             this.server.TabIndex = 5;
             this.server.Text = "3.128.144.165";
             // 
             // bd2
             // 
-            this.bd2.Location = new System.Drawing.Point(169, 339);
+            this.bd2.Location = new System.Drawing.Point(113, 220);
+            this.bd2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bd2.Name = "bd2";
-            this.bd2.Size = new System.Drawing.Size(213, 26);
+            this.bd2.Size = new System.Drawing.Size(143, 20);
             this.bd2.TabIndex = 6;
             this.bd2.Text = "DB20212000317";
             // 
             // user
             // 
-            this.user.Location = new System.Drawing.Point(169, 390);
+            this.user.Location = new System.Drawing.Point(113, 253);
+            this.user.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.user.Name = "user";
-            this.user.Size = new System.Drawing.Size(213, 26);
+            this.user.Size = new System.Drawing.Size(143, 20);
             this.user.TabIndex = 7;
             this.user.Text = "Evelyn.Sabillon";
             // 
             // pw
             // 
-            this.pw.Location = new System.Drawing.Point(169, 437);
+            this.pw.Location = new System.Drawing.Point(113, 284);
+            this.pw.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pw.Name = "pw";
-            this.pw.Size = new System.Drawing.Size(213, 26);
+            this.pw.Size = new System.Drawing.Size(143, 20);
             this.pw.TabIndex = 8;
             this.pw.Text = "ES20212000317";
             // 
@@ -134,9 +143,10 @@
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(224)))), ((int)(((byte)(254)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(28, 495);
+            this.button1.Location = new System.Drawing.Point(19, 322);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 57);
+            this.button1.Size = new System.Drawing.Size(104, 37);
             this.button1.TabIndex = 9;
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = true;
@@ -148,20 +158,21 @@
             this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(224)))), ((int)(((byte)(254)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.SystemColors.Control;
-            this.button2.Location = new System.Drawing.Point(217, 495);
+            this.button2.Location = new System.Drawing.Point(145, 322);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 56);
+            this.button2.Size = new System.Drawing.Size(99, 36);
             this.button2.TabIndex = 10;
-            this.button2.Text = "Cancelar";
+            this.button2.Text = "PruebaKenneth";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(426, 591);
+            this.ClientSize = new System.Drawing.Size(284, 384);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pw);
@@ -173,6 +184,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Login";
