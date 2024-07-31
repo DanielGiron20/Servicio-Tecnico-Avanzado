04eb1f7010d1735d0267b0dd24ea2c5215fcd478
Kenneth <kennethmendez702@gmail.com>
2024-07-30 19:48:59 -0600

    Arreglando lo que hice en la prueba.

--- Proyecto DB2/Login.Designer.cs
+++ Proyecto DB2/Login.Designer.cs
@@ -163,7 +163,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(99, 36);
             this.button2.TabIndex = 10;
-            this.button2.Text = "PruebaKenneth";
+            this.button2.Text = "Cancelar";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
